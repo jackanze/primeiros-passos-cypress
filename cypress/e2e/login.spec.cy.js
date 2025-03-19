17e5333e05a4385d1ec06ee71b25dfe6f7c3c357
jackanze <jackanze@gmail.com>
2025-03-19 20:15:13 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
@@ -6,17 +8,6 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
-  }
-
-  const userData = {
-    userSuccess: {
-      username: 'Admin',
-      password: 'admin123'
-    },
-    userFail: {
-      username: 'Test',
-      password: 'test'
-    }
   }
 
   it('Login - Success', () => {
